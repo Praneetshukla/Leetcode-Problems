fefc8a7cc2922bf978f4301c41a1e26d9e53b56e
Praneet Shukla <praneetshukla601@gmail.com>
2026-08-28 21:55:57 +0530

    Time: 0 ms (100%), Space: 10.6 MB (89.23%) - LeetHub

--- 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
+++ 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
@@ -1,94 +1,104 @@
+int8_t dp[151];
 class Solution {
 public:
-    string isPossible(int n, vector<int> freq, string cur, char &mid, string& target){
-        // this gives the max palindrome achievable with the current prefix 
-        //(descending order)
-        for(int i=25; i>=0; i--){
-            while(freq[i]){
-                cur += (char)('a'+i);
-                freq[i]--;
-            }
+    array<int, 26> freq{};
+    int n, half;
+    int oddIdx=-1;
+    bool revLeftIsGrR(const string& t){
+        for(int i=0; i<half; i++){
+            if (t[half-1-i]>t[half+(n&1)+i]) return 1;
+            if (t[half-1-i]<t[half+(n&1)+i]) return 0;
         }
-        if(mid!='#'){
-            // odd-length palindrome: left half + mid + reverse(left half)
-            string temp = cur;
-            cur += mid;
-            reverse(temp.begin(), temp.end());
-            cur.append(temp.begin(), temp.end());
-        }
-        else {
-            // even-length palindrome: left half + reverse(left half)
-            string temp = cur;
-            reverse(temp.begin(), temp.end());
-            cur.append(temp.begin(), temp.end());
-        }
-        // feasibility check: only valid if this (largest possible) candidate beats target
-        return cur>target? cur : "";
+        return 0;
     }
+    // Assume i < half
+    bool canPlace(int i, const string& target, unsigned hasC) {
+        if (dp[i]!=-1) return dp[i];
+        const char c=target[i];
+        if (freq[c-'a']==0) return 0;
 
-    string lexPalindromicPermutation(string s, string target) {
-        int n = s.size();
-        vector<int> freq(26, 0);
-        if(n==1){
-            // if size is 1 then direct compare
-            if(s>target) return s;
-            else return "";
+        // Use 1 c
+        unsigned hasC1=hasC;
+        if (--freq[c-'a']==0) hasC1 &= ~(1<<(c-'a'));
+        
+        bool ans=0;
+        if (i==half-1) {
+            if (n&1) {
+                if ('a'+oddIdx!=target[half]) 
+                    ans=('a'+oddIdx > target[half]);
+                else ans=revLeftIsGrR(target);
+            } 
+            else ans=revLeftIsGrR(target);
+        } 
+        else {
+            int nxt=target[i+1]-'a';
+            if (hasC1>>(nxt+1))
+                ans=1;
+            else if (!((hasC1>>nxt) & 1)) 
+                ans=0;
+            else 
+                ans=canPlace(i+1, target, hasC1);
         }
-        for(char c : s) freq[c-'a']++; // freq Track
+        // Backtrack readd 1 c
+        freq[c-'a']++;
+        return dp[i]=ans;
+    }
+    inline string build_palindrome(const string& ans){
+        string pal=ans;
+         if (n&1) pal+='a'+oddIdx;
+        string rev=ans;
+        reverse(rev.begin(), rev.end());
+        pal+=rev;
+        return pal;
+    }
+    string lexPalindromicPermutation(string& s, string& target) {
+        n=s.size(), half=n>>1;
+        memset(dp, -1, half);
+        unsigned par=0, hasC=0;
+        for(char c : s){
+            const int idx=c-'a';
+            freq[idx]++;
+            hasC|=1<<idx;
+            par^=(1<<idx);
+        }
+        if (popcount(par)>1) return "";
 
-        char mid = '#';
-        int oddCount = 0;
-
-        for(int i=0; i<26; i++){
-            if(freq[i]%2){
-                // odd count -> this becomes the middle character
-                mid = (char)('a'+i);
-                freq[i]--;
-                oddCount++;
-            }
-
-            freq[i] /= 2; // each char used freq[i]/2 times in the left half
-
-            if(oddCount>=2) return ""; // more than one odd-frequency char -> can't form a palindrome
+        if (n&1) oddIdx=countr_zero(par);
+        for(unsigned mask=hasC; mask; mask&=(mask-1)){
+            int i=countr_zero(mask);
+            freq[i]>>=1;
+            // original oddIdx may freq=1
+            if (freq[i]==0) hasC &=~(1u<<i);
         }
 
-        n /= 2; // we only need to construct the left half now
-        string res = "", prefix = "";
+        string ans="";
+        for (int i=0; i<half; i++) {
+            int t_i=target[i]-'a';
+            // Try to match target[i] if valid
+            if (freq[t_i] > 0 && canPlace(i,target, hasC)) {
+                ans+=target[i];
+                if (--freq[t_i]==0) hasC &= ~(1u<<t_i); 
+            } 
+            else {
+                // Use bitmask to find the min char>target[i]
+                unsigned higher=hasC>>(t_i+1);
+                if (higher==0) return "";
+                int choice=countr_zero(higher)+t_i+1;
+                if (--freq[choice]==0) hasC &= ~(1u<<choice);
+                ans+='a'+ choice;
 
-        // greedily build the left half, position by position
-        for(int i=0; i<n; i++){
-
-            string cur = prefix;
-            bool isThereAny = false;
-
-            // try smallest character first ('a' -> 'z')
-            for(int j=0; j<26; j++){
-                if(freq[j]){
-                    freq[j]--;
-                    cur += (char)('a'+j);
-
-                    // check if this prefix can still lead to a palindrome > target
-                    string isPos = isPossible(n, freq, cur, mid, target);
-
-                    if(isPos!=""){
-                        prefix = cur;      // keep this character, lock in the prefix
-                        isThereAny = true;
-
-                        if(res=="") res = isPos;
-                        else res = min(res, isPos); // track smallest valid candidate seen
-                        break;
-                    }
-
-                    // this character doesn't work, undo and try the next one
-                    freq[j]++;
-                    cur.pop_back();
+                //remaining positions  with smallest available characters
+                for (int j=i+1; j<half; j++) {
+                    int idx=countr_zero(hasC);
+                    ans+='a'+idx;
+                    if (--freq[idx]==0) hasC &=~(1u<<idx); 
                 }
+                return build_palindrome(ans);
             }
-
-            if(!isThereAny)
-                return ""; // no character works at this position -> impossible
         }
 
-        return res; 
+        // Prefix up to half matches target[0...half-1]
+        string pal=build_palindrome(ans);
+        return pal>target ? pal : "";
     }
 };
